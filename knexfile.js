73acd1f4fd5420016098828cc50a62e5798a0b83
zagodi <diego.s.zago1995@gmail.com>
2020-10-09 18:23:06 -0300

    using knex

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: "dbexample",
+      user: "diegozago",
+      password: "12345678"
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  },
+};
